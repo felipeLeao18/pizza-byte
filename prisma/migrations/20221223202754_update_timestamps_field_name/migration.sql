10b07ea6b623de39e34d0b6ec1922a064a5f047f
felipeLeao18 <flpleao1807@gmail.com>
2022-12-23 17:29:14 -0300

    style: update timestamps fields to camel case

---
+++ prisma/migrations/20221223202754_update_timestamps_field_name/migration.sql
@@ -0,0 +1,44 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `created_at` on the `categories` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `categories` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `items` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `items` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `orders` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `orders` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `products` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `products` table. All the data in the column will be lost.
+  - You are about to drop the column `created_at` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "categories" DROP COLUMN "created_at",
+DROP COLUMN "updated_at",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "items" DROP COLUMN "created_at",
+DROP COLUMN "updated_at",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "orders" DROP COLUMN "created_at",
+DROP COLUMN "updated_at",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "products" DROP COLUMN "created_at",
+DROP COLUMN "updated_at",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "created_at",
+DROP COLUMN "updated_at",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;

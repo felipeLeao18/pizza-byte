39e3c6411c544e9190b4f83fe100d2cb6f1101f7
felipeLeao18 <flpleao1807@gmail.com>
2022-12-24 19:11:17 -0300

    feat: add cryptography module

---
+++ src/infra/cryptography/cryptography.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { BcryptRepository } from './bcrypt/bcrypt-repository';
+
+@Module({
+  exports: [BcryptRepository],
+})
+export class DatabaseModule {}

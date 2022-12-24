27cd79983238cfbb8cc3ff817f34cbab122ac884
felipeLeao18 <flpleao1807@gmail.com>
2022-12-24 19:13:04 -0300

    feat: implement findOneByEmail in db memory repository

--- src/app/repositories/in-memory-user-repository.ts
+++ src/app/repositories/in-memory-user-repository.ts
@@ -3,6 +3,13 @@ import { UserRepository } from './user-repository';
 
 export class InMemoryUserRepository extends UserRepository {
   public users: User[] = [];
+  async findOneByEmail(email: string): Promise<Partial<User> | null> {
+    const user = await this.users.find((user) => user.email.value === email);
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
   async signup(user: User): Promise<User> {
     const usersCount = this.users.push(user);
     return this.users[usersCount - 1];

b9a43d10277e8e9460535073305da56d32788ef6
felipeLeao18 <flpleao1807@gmail.com>
2022-12-24 19:12:23 -0300

    feat: add findOneByEmail

--- src/app/repositories/user-repository.ts
+++ src/app/repositories/user-repository.ts
@@ -2,4 +2,5 @@ import { User } from '@app/entities/user';
 
 export abstract class UserRepository {
   abstract signup(user: User): Promise<User>;
+  abstract findOneByEmail(email: string): Promise<Partial<User> | null>;
 }

8e24262d718e2ceea0df158c8f0357e8ce37abde
felipeLeao18 <flpleao1807@gmail.com>
2022-12-25 23:06:23 -0300

    feat: add find by id to in memory user repo

--- src/test/repositories/in-memory-user-repository.ts
+++ src/test/repositories/in-memory-user-repository.ts
@@ -3,14 +3,23 @@ import { UserRepository } from '@app/repositories/user-repository';
 
 export class InMemoryUserRepository extends UserRepository {
   public users: User[] = [];
-  async findOneByEmail(email: string): Promise<Partial<User> | null> {
+  async findById(id: string): Promise<User | null> {
+    const user = await this.users.find((user) => user.id === id);
+
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
+
+  async findOneByEmail(email: string): Promise<User | null> {
     const user = await this.users.find((user) => user.email.value === email);
     if (!user) {
       return null;
     }
     return user;
   }
-  async signup(user: User): Promise<User> {
+  async create(user: User): Promise<User> {
     const usersCount = this.users.push(user);
     return this.users[usersCount - 1];
   }

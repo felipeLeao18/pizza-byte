783cbbb06891fd7cc67eee4548e15bc5b872bb07
felipeLeao18 <flpleao1807@gmail.com>
2022-12-25 23:06:02 -0300

    feat: add login to http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,5 +1,8 @@
+import { AuthenticatorRepository } from '@app/repositories/authenticator-repository';
 import { CrypterRepository } from '@app/repositories/crypter-repository';
+import { Login } from '@app/use-cases/user/login';
 import { SignUp } from '@app/use-cases/user/signup';
+import { JwtRepository } from '@infra/authentication/jwt/jwt.repository';
 import { BcryptRepository } from '@infra/cryptography/bcrypt/bcrypt-repository';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
@@ -13,6 +16,15 @@ import { UserController } from './controllers/user-controller';
     {
       provide: CrypterRepository,
       useClass: BcryptRepository,
+    },
+    Login,
+    {
+      provide: CrypterRepository,
+      useClass: BcryptRepository,
+    },
+    {
+      provide: AuthenticatorRepository,
+      useClass: JwtRepository,
     },
   ],
 })

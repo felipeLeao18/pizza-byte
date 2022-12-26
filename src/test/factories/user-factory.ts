db336fd68b62afbe3101ef344fac91ca86d04d18
felipeLeao18 <flpleao1807@gmail.com>
2022-12-26 19:51:52 -0300

    feat: add user from factory to be returned as instance of user entity

--- src/test/factories/user-factory.ts
+++ src/test/factories/user-factory.ts
@@ -1,13 +1,13 @@
 import { Email } from '@app/entities/email';
 import { Password } from '@app/entities/password';
-import { IUserProps } from '@app/entities/user';
+import { IUserProps, User } from '@app/entities/user';
 
 type Override = Partial<IUserProps>;
 export function makeUser(override?: Override) {
-  return {
+  return new User({
     email: new Email('factory_user@mail.com'),
     name: 'factory_name',
     password: new Password('factory_password'),
     ...override,
-  };
+  });
 }

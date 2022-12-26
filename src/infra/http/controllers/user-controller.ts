489db69d34bc2dd921aaa0c384781e180312d12d
felipeLeao18 <flpleao1807@gmail.com>
2022-12-25 23:05:27 -0300

    feat: add login to user controller

--- src/infra/http/controllers/user-controller.ts
+++ src/infra/http/controllers/user-controller.ts
@@ -1,3 +1,4 @@
+import { Login } from '@app/use-cases/user/login';
 import { SignUp } from '@app/use-cases/user/signup';
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserBody } from '../dtos/create-user-body';
@@ -5,7 +6,7 @@ import { UserViewModel } from '../view-models/user-view-model';
 
 @Controller('user')
 export class UserController {
-  constructor(private signup: SignUp) {}
+  constructor(private signup: SignUp, private _login: Login) {}
 
   @Post()
   async signUp(@Body() body: CreateUserBody) {
@@ -17,5 +18,16 @@ export class UserController {
     });
 
     return { user: UserViewModel.toHTTP(user) };
+  }
+
+  @Post('/login')
+  async login(@Body() body: CreateUserBody) {
+    const { email, password } = body;
+    const { user, token } = await this._login.execute({
+      email,
+      password,
+    });
+
+    return { user: UserViewModel.toHTTP(user), token };
   }
 }

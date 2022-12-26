7bbab0ae37ba48489df4a6b1a9f0392fcbb1bd19
felipeLeao18 <flpleao1807@gmail.com>
2022-12-25 23:05:07 -0300

    feat: add jwt adapter

---
+++ src/infra/authentication/jwt/jwt.repository.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { AuthenticatorRepository } from '@app/repositories/authenticator-repository';
+import jwt from 'jsonwebtoken';
+import { config } from 'src/config';
+
+export class JwtRepository implements AuthenticatorRepository {
+  constructor() {}
+
+  private readonly secret = config.JWT_SECRET;
+  async encrypt(plaintext: string): Promise<string> {
+    return jwt.sign({ id: plaintext }, this.secret);
+  }
+
+  async decrypt(ciphertext: string): Promise<string> {
+    return jwt.verify(ciphertext, this.secret) as any;
+  }
+}

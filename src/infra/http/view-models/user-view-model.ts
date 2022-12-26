2cadd23fa58f98d525e597c49d8d5354890b628d
felipeLeao18 <flpleao1807@gmail.com>
2022-12-25 23:05:55 -0300

    fix: add toHttp view model to be able to remove parsed data

--- src/infra/http/view-models/user-view-model.ts
+++ src/infra/http/view-models/user-view-model.ts
@@ -1,13 +1,21 @@
 import { User } from '@app/entities/user';
 
 export class UserViewModel {
-  static toHTTP(user: User) {
-    return {
+  static toHTTP(user: Partial<User>) {
+    const userMade = {
       id: user.id,
-      email: user.email.value,
+      email: user.email?.value,
       name: user.name,
       createdAt: user.createdAt,
       updatedAt: user.updatedAt,
     };
+
+    Object.keys(userMade).filter((key) => {
+      if (userMade[key] === null) {
+        delete userMade[key];
+      }
+    });
+
+    return userMade;
   }
 }

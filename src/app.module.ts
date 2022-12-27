5e2c62a5883b143c891caf55713c1e4f175623e1
felipeLeao18 <flpleao1807@gmail.com>
2022-12-27 01:05:01 -0300

    feat: add middleware to category controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
+import { GetUserByToken } from '@app/use-cases/user/get-user-by-token';
 import { DatabaseModule } from '@infra/database/database.module';
+import { CategoryController } from '@infra/http/controllers/category-controller';
 import { HttpModule } from '@infra/http/http.module';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 @Module({
   imports: [DatabaseModule, HttpModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(GetUserByToken).forRoutes(CategoryController);
+  }
+}

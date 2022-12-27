f5ce686c1ebe91145850f0f28779ef2881cbb320
felipeLeao18 <flpleao1807@gmail.com>
2022-12-27 01:03:38 -0300

    feat: add decoded from jwt adapter

--- src/infra/authentication/jwt/jwt.repository.ts
+++ src/infra/authentication/jwt/jwt.repository.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { AuthenticatorRepository } from '@app/repositories/authenticator-repository';
+import { Injectable } from '@nestjs/common';
 import jwt from 'jsonwebtoken';
 import { config } from 'src/config';
 
+@Injectable()
 export class JwtRepository implements AuthenticatorRepository {
   constructor() {}
 
@@ -12,6 +14,11 @@ export class JwtRepository implements AuthenticatorRepository {
   }
 
   async decrypt(ciphertext: string): Promise<string> {
-    return jwt.verify(ciphertext, this.secret);
+    const decoded = jwt.verify(ciphertext, this.secret);
+    if (typeof decoded === 'object' && decoded.id) {
+      return decoded.id;
+    }
+
+    return decoded as string;
   }
 }

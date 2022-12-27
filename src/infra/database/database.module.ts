9af4ba839263069064ac2b646ef6e6cfbe09a8de
felipeLeao18 <flpleao1807@gmail.com>
2022-12-27 01:03:53 -0300

    feat: add database module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,6 +1,8 @@
+import { CategoryRepository } from '@app/repositories/category-repository';
 import { UserRepository } from '@app/repositories/user-repository';
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaCategoryRepository } from './prisma/repositories/prisma-category-repository';
 import { PrismaUserRepository } from './prisma/repositories/prisma-user-repository';
 
 @Module({
@@ -10,7 +12,11 @@ import { PrismaUserRepository } from './prisma/repositories/prisma-user-reposito
       provide: UserRepository,
       useClass: PrismaUserRepository,
     },
+    {
+      provide: CategoryRepository,
+      useClass: PrismaCategoryRepository,
+    },
   ],
-  exports: [UserRepository],
+  exports: [UserRepository, CategoryRepository],
 })
 export class DatabaseModule {}

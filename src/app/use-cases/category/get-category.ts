424370aa239aeb79c00f9dd62e4b4d1e14af1242
felipeLeao18 <flpleao1807@gmail.com>
2022-12-27 21:15:46 -0300

    feat: ensure category can be found

---
+++ src/app/use-cases/category/get-category.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { CategoryRepository } from '@app/repositories/category-repository';
+import { Category } from '@app/entities/category';
+import { CategoryNotFoundError } from './errors/category-errors';
+
+interface IGetCategoryRequest {
+  id: string;
+}
+
+interface IGetCategoryResponse {
+  category: Category;
+}
+
+@Injectable()
+export class GetCategoryById {
+  constructor(private categoryRepository: CategoryRepository) {}
+  async execute(request: IGetCategoryRequest): Promise<IGetCategoryResponse> {
+    const { id } = request;
+
+    const category = await this.categoryRepository.findById(id);
+
+    if (!category) {
+      throw new CategoryNotFoundError();
+    }
+
+    return { category };
+  }
+}

77c832f7113e2e9272d9622f9a1b85bf5eaf4064
felipeLeao18 <flpleao1807@gmail.com>
2022-12-27 21:15:15 -0300

    feat: add find by id to category interface

--- src/app/repositories/category-repository.ts
+++ src/app/repositories/category-repository.ts
@@ -2,4 +2,5 @@ import { Category } from '@app/entities/category';
 
 export abstract class CategoryRepository {
   abstract create(category: Category): Promise<Category>;
+  abstract findById(id: string): Promise<Category | null>;
 }

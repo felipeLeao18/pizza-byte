a339d56add80c4a2bffce794f28da3dc2283aca8
felipeLeao18 <flpleao1807@gmail.com>
2022-12-27 21:18:58 -0300

    feat: add get category by id route

--- src/infra/http/controllers/category-controller.ts
+++ src/infra/http/controllers/category-controller.ts
@@ -1,14 +1,25 @@
-import { AuthenticatorRepository } from '@app/repositories/authenticator-repository';
 import { CreateCategory } from '@app/use-cases/category/create-category';
+import { GetCategoryById } from '@app/use-cases/category/get-category';
 import { MyMiddlewareProvider } from '@infra/middlewares/middleware.service';
-import { Body, Controller, Post, Req, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateCategoryBody } from '../dtos/user/create-category-body';
 import { CategoryViewModel } from '../view-models/category-view-model';
 
 @UseInterceptors(MyMiddlewareProvider)
 @Controller('categories')
 export class CategoryController {
-  constructor(private _create: CreateCategory) {}
+  constructor(
+    private _create: CreateCategory,
+    private _getById: GetCategoryById,
+  ) {}
 
   @Post()
   async create(@Body() body: CreateCategoryBody) {
@@ -16,6 +27,15 @@ export class CategoryController {
 
     const { category } = await this._create.execute({
       name,
+    });
+
+    return { category: CategoryViewModel.toHTTP(category) };
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: string) {
+    const { category } = await this._getById.execute({
+      id,
     });
 
     return { category: CategoryViewModel.toHTTP(category) };

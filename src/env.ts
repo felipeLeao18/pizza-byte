8fd85e6926f852826ee1cfcd80867a937255e735
felipeLeao18 <flpleao1807@gmail.com>
2022-12-31 16:32:45 -0300

    chore: add aws keys to s3

--- src/env.ts
+++ src/env.ts
@@ -3,4 +3,7 @@ dotenv.config();
 
 export const config = {
   JWT_SECRET: process.env.JWT_SECRET ?? '5ce5ecba-608a-4561-932a-05e25b86f672',
+  AWS_S3_BUCKET: process.env.AWS_S3_BUCKET,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
 };

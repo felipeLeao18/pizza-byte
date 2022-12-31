c1669e088ccd6003fc14bc1bbe2d8095d5f1ad9c
felipeLeao18 <flpleao1807@gmail.com>
2022-12-31 16:31:55 -0300

    fix: remove get link from s3 adapter

--- src/infra/bucket/s3/s3.repository.ts
+++ src/infra/bucket/s3/s3.repository.ts
@@ -3,13 +3,50 @@ import { BucketRepository } from '@app/repositories/bucket-repository';
 import { Injectable } from '@nestjs/common';
 import { config } from 'src/env';
 
+import AWS, { S3 } from 'aws-sdk';
+import { randomUUID } from 'crypto';
+
 @Injectable()
 export class S3Repository implements BucketRepository {
   constructor() {}
-  async upload(blob: any): Promise<{ link: string }> {
-    throw new Error('Method not implemented.');
+
+  private s3: S3;
+  private readonly bucket = config.AWS_S3_BUCKET;
+
+  prepare() {
+    AWS.config.update({
+      accessKeyId: config.AWS_ACCESS_KEY_ID,
+      secretAccessKey: config.AWS_SECRET_ACCESS_KEY,
+    });
   }
-  async getLink(blobId: string): Promise<{ link: string }> {
-    throw new Error('Method not implemented.');
+
+  private setInstance() {
+    this.prepare();
+    this.s3 = new AWS.S3();
+  }
+
+  private getInstance() {
+    return this.s3;
+  }
+
+  async upload(blob: any): Promise<{ link: string }> {
+    this.setInstance();
+    const params = {
+      ACL: 'public-read',
+      Bucket: this.bucket as string,
+      Key: blob.originalname.concat(randomUUID()),
+      Body: blob.buffer,
+      ContentType: blob.mimetype,
+    };
+
+    this.s3.upload(params, (err, data) => {
+      if (err) {
+        throw new Error('error uploading file', { cause: err });
+      }
+      return { link: data.location };
+    });
+    return {
+      link: `https://${config.AWS_S3_BUCKET}.s3.amazonaws.com/${params.Key}`,
+    };
   }
 }

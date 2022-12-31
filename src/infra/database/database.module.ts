9cbb4db40c8ef85c589089b22b1e9b661028fded
felipeLeao18 <flpleao1807@gmail.com>
2022-12-31 16:34:02 -0300

    feat: add product repository and prisma service to be exported

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,10 @@
 import { CategoryRepository } from '@app/repositories/category-repository';
+import { ProductRepository } from '@app/repositories/product-repository';
 import { UserRepository } from '@app/repositories/user-repository';
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaCategoryRepository } from './prisma/repositories/prisma-category-repository';
+import { PrismaProductRepository } from './prisma/repositories/prisma-product-repository';
 import { PrismaUserRepository } from './prisma/repositories/prisma-user-repository';
 
 @Module({
@@ -16,7 +18,16 @@ import { PrismaUserRepository } from './prisma/repositories/prisma-user-reposito
       provide: CategoryRepository,
       useClass: PrismaCategoryRepository,
     },
+    {
+      provide: ProductRepository,
+      useClass: PrismaProductRepository,
+    },
   ],
-  exports: [UserRepository, CategoryRepository],
+  exports: [
+    UserRepository,
+    CategoryRepository,
+    ProductRepository,
+    PrismaService,
+  ],
 })
 export class DatabaseModule {}

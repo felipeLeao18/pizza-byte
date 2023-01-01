d374d61c20d966cf9de0c95c76919cdf4015fb0a
felipeLeao18 <flpleao1807@gmail.com>
2023-01-01 12:58:29 -0300

    feat: add get many to local db products

--- src/test/repositories/in-memory-product-repository.ts
+++ src/test/repositories/in-memory-product-repository.ts
@@ -7,4 +7,14 @@ export class InMemoryProductRepository extends ProductRepository {
     const productsCount = this.products.push(product);
     return this.products[productsCount - 1];
   }
+
+  async getMany(category_id?: string): Promise<Product[]> {
+    if (!category_id) {
+      return this.products;
+    }
+
+    return this.products.filter(
+      (product: Product) => product.category_id === category_id,
+    );
+  }
 }

93d0ec064dc5d74f469ecc5b5e0bd280495cb550
felipeLeao18 <flpleao1807@gmail.com>
2023-01-01 12:57:09 -0300

    feat: add get many to product repository

--- src/app/repositories/product-repository.ts
+++ src/app/repositories/product-repository.ts
@@ -2,4 +2,5 @@ import { Product } from '@app/entities/product';
 
 export abstract class ProductRepository {
   abstract create(product: Product): Promise<Product>;
+  abstract getMany(categoryId?: string): Promise<Product[]>;
 }

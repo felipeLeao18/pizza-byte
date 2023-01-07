cc7bc9cb1f537cf9db90c4626e9e709adf0ed39b
felipeLeao18 <flpleao1807@gmail.com>
2023-01-07 14:01:00 -0300

    feat: add get user logged info to user controller

--- src/infra/http/controllers/user-controller.ts
+++ src/infra/http/controllers/user-controller.ts
@@ -1,13 +1,18 @@
+import { GetUserLoggedInfo } from '@app/use-cases/user/get-user-logged-info';
 import { Login } from '@app/use-cases/user/login';
 import { SignUp } from '@app/use-cases/user/signup';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Query } from '@nestjs/common';
 import { CreateUserBody } from '../dtos/user/create-user-body';
 import { LoginBody } from '../dtos/user/login-body';
 import { UserViewModel } from '../view-models/user-view-model';
 
 @Controller('user')
 export class UserController {
-  constructor(private signup: SignUp, private _login: Login) {}
+  constructor(
+    private signup: SignUp,
+    private _login: Login,
+    private _getUserLoggedInfo: GetUserLoggedInfo,
+  ) {}
 
   @Post()
   async signUp(@Body() body: CreateUserBody) {
@@ -21,6 +26,7 @@ export class UserController {
     return { user: UserViewModel.toHTTP(user) };
   }
 
+  @HttpCode(200)
   @Post('/login')
   async login(@Body() body: LoginBody) {
     const { email, password } = body;
@@ -30,5 +36,16 @@ export class UserController {
     });
 
     return { user: UserViewModel.toHTTP(user), token };
+  }
+
+  @HttpCode(200)
+  @Post('/me')
+  async getUserLoggedInfo(@Query() query: { userId: string }) {
+    const { userId } = query;
+    const { user } = await this._getUserLoggedInfo.execute({
+      userId,
+    });
+
+    return { user: UserViewModel.toHTTP(user) };
   }
 }
